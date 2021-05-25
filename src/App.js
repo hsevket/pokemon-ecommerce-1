88edcacc021cbb1c7a421bb3dbe87158a9851dff
Ivan Timofeev <ivan.timofeev@sixt.com>
2021-05-25 21:15:26 +0200

    work progress during lesson

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Overview } from "./Overview/Overview";
+import { Overview } from "./Overview";
 import { Checkout } from "./Checkout/Checkout";
 import { Details } from "./Details/Details";
 import { OrderCompleted } from "./OrderCompleted/OrderCompleted";
@@ -14,6 +14,23 @@ const Title = styled.h1`
 `;
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+      .then(( { results } ) => results.map((pokemon, index) => ({
+        ...pokemon,
+        price: (Math.random() * index + 1).toFixed(2)
+      })))
+      .then((data) => {
+        setPokemons(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+  }, []);
+
   return (
     <Router>
       <div>
@@ -39,7 +56,9 @@ export default function App() {
         <Title>Pokemon ecommerce</Title>
         <Switch>
           <Route exact path="/">
-            <Overview />
+            <Overview
+              data={pokemons}
+            />
           </Route>
           <Route path="/shopping-cart">
             <ShoppingCart />
--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -1,3 +1,79 @@
-export function Overview() {
-  return <div>Overview</div>;
+import { useState, useMemo } from 'react';
+import styled from 'styled-components';
+import { useHistory } from "react-router-dom";
+
+import { PokemonCard } from "../common/PokemonCard"
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 50px auto;
+  padding: 25px 300px;
+  grid-template-columns: auto;
+  grid-gap: 1rem;
+`;
+
+const CardsWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 340px);
+  grid-gap: 1rem;
+  justify-content: center;
+`;
+
+const FiltersContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: auto 100px;
+  grid-gap: 1rem;
+  justify-content: center;
+`;
+
+const Filter = styled.input`
+  font-size: 23px;
+`;
+
+export function Overview({ data }) {
+  console.log('DATA in overview', data);
+  const history = useHistory();
+  const [filter, setFilter] = useState('');
+
+  const filteredPokemons = useMemo(() => (
+    data.filter((pokemon) => pokemon.name.includes(filter.toLowerCase()))
+  ), [filter, data])
+
+  const handleCardClick = (id) => {
+    history.push(`/${id}`);
+  }
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  }
+
+  return (
+    <Container>
+      <FiltersContainer>
+        <Filter
+          type="text"
+          placeholder="Filter pokemons here"
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </FiltersContainer>
+      <CardsWrapper>
+        {
+          filteredPokemons.map(({ name, url, price }) => (
+            <PokemonCard
+              key={name}
+              name={name}
+              price={price}
+              image={`https://pokeres.bastionbot.org/images/pokemon/${
+                url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '')
+              }.png`}
+              click={() => handleCardClick(url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', ''))}
+            />
+          ))
+        }
+      </CardsWrapper>
+    </Container>
+  );
 }
